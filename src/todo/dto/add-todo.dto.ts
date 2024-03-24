4dd6f42be741ce23ebc9b51ed6a1e56b742a7367
ahmedsilinii <ahmedsilini5@gmail.com>
2024-03-24 18:41:28 +0100

    class validator added

--- src/todo/dto/add-todo.dto.ts
+++ src/todo/dto/add-todo.dto.ts
@@ -1,4 +1,19 @@
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class AddTodoDto{
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(
+        6,
+        {
+            message : 'La taille min est 6 char'
+        },
+     )
     name : string;
+
+    @IsString()
+    @IsNotEmpty()
     desc : string;
+
+     
 }
