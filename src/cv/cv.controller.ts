fc9ca347a70215b48cc916a4c7dc86de263ff16d
ahmedsilinii <ahmedsilini5@gmail.com>
2024-03-26 18:12:48 +0100

    Update cv.controller.ts

--- src/cv/cv.controller.ts
+++ src/cv/cv.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import CvEntity from './entities/cv.entity/cv.entity';
+import { CvService } from './cv.service';
 
 @Controller('cv')
-export class CvController {}
+export class CvController {
+    constructor(
+        private readonly cvService: CvService
+    ){}
+
+
+    @Get()
+    getAllCvs(): Promise<CvEntity[]>{
+        return this.cvService.getCvs();
+    }
+
+}
