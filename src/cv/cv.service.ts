a724a09ca58bd1dea65e9921f4707d9f166aaca2
ahmedsilinii <ahmedsilini5@gmail.com>
2024-03-26 18:02:56 +0100

    intro to repository

--- src/cv/cv.service.ts
+++ src/cv/cv.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import CvEntity from './entities/cv.entity/cv.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CvService {}
+export class CvService {
+    constructor (
+        @InjectRepository(CvEntity)
+        private cvRepository: Repository<CvEntity>
+    ) {}   
+    
+    async getCvs(): Promise<CvEntity[]> {
+        return await this.cvRepository.find();
+    }
+}
+
