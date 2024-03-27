56353a62c3bdd2bb1ae1fb3a324c73faccfc5d74
ahmedsilinii <ahmedsilini5@gmail.com>
2024-03-27 14:53:25 +0100

    passport strategy

---
+++ src/user/strategy/passport-jwt.strategy.ts
@@ -0,0 +1,35 @@
+import { PassportStrategy,  } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExtractJwt,Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { PayloadInterface } from '../interfaces/payload-interface';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private userRepository : Repository<UserEntity>
+  )
+    {
+    super({
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        ignoreExpiration: false,
+        secretOrKey: configService.get('SECRET')
+    });
+  }
+
+    async validate(payload: PayloadInterface) {
+        const user = await this.userRepository.findOne({
+            where: { username: payload.username }
+        });
+
+        if(user){
+            const {password, salt, ...result} = user;
+            return result;
+        }else{
+            throw new UnauthorizedException();
+        }
+    }
+}
