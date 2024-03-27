a857c8c977c0b074cf17ff4266cd1389f7f2b360
ahmedsilinii <ahmedsilini5@gmail.com>
2024-03-27 14:23:49 +0100

    jwt config in module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,24 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
+import { Passport } from 'passport';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Module({
   imports:[
-    TypeOrmModule.forFeature([UserEntity])
+    TypeOrmModule.forFeature([UserEntity]),
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
+    JwtModule.register({
+      secret: process.env.SECRET,
+      signOptions: {
+        expiresIn: '1d'
+      }
+    }),
   ],
   controllers: [UserController],
   providers: [UserService]
